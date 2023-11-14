7a123dc704486ebfde1313f78013588d50e2c7c1
Shivakumar A S <shivakumar42as@gmail.com>
2023-11-14 22:42:44 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ PattrenTri.java
@@ -0,0 +1,15 @@
+package com.kodnest.pattrean;
+
+public class PattrenTri {
+
+	public static void main(String[] args) {
+		for(int i=1;i<=4;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}

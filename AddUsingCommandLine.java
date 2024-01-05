751a031a6f7cb008b3449ca52e07f39587c7f6f5
Shivakumar A S <shivakumar42as@gmail.com>
2024-01-05 09:02:13 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ AddUsingCommandLine.java
@@ -0,0 +1,13 @@
+package com.kodnest.level1;
+
+public class AddUsingCommandLine {
+
+	public static void main(String[] a) {
+		int A=Integer.parseInt(a[0]);
+		int B=Integer.parseInt(a[1]);
+		int c=A+B;
+		System.out.println(c);
+
+	}
+
+}

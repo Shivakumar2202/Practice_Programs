e65c55ce082dc9a5285623c3ac957904017bb957
Shivakumar A S <shivakumar42as@gmail.com>
2024-01-18 21:04:47 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ Recursion.java
@@ -0,0 +1,16 @@
+package String_progremms.level1;
+
+public class Recursion {
+	void recursion(int a) {
+		
+		System.out.println("HELLO!");
+		a--;
+		if(a>0) {
+		//return;
+    	//}
+		//else {
+		recursion(a);
+		}
+	}
+
+}
---
+++ RecursionCall.java
@@ -0,0 +1,12 @@
+package String_progremms.level1;
+
+public class RecursionCall {
+
+	public static void main(String[] args) {
+		Recursion display=new Recursion();
+		int count=5;
+		display.recursion(count);
+
+	}
+
+}

17c3d51c5cc11b10dbce58841fdc54218db54f06
Shivakumar A S <shivakumar42as@gmail.com>
2024-01-22 22:48:04 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ RemoveDupelicate.java
@@ -0,0 +1,13 @@
+package String_progremms.level1;
+
+public class RemoveDupelicate {
+
+	public static void main(String[] args) {
+		String s="program";
+		char c[]=new char[s.length()];
+		for(int i=0;i<s.length();i++) {
+			c[i]=s.charAt(i);
+		}
+	}
+
+}

045641f03867d46a9df64f6009b663d119ce7ea9
Shivakumar A S <shivakumar42as@gmail.com>
2024-01-28 23:19:16 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ String2.java
@@ -0,0 +1,19 @@
+package String_progremms.level1;
+
+public class String2 {
+
+	public static void main(String[] args) {
+		String s1="123";
+		String s2="456";
+		String s4=new String("java");
+		String s3=new String("123");
+		//System.out.println(s1==s2);
+		//System.out.println(s1==s3);
+		//System.out.println(s3==s4);
+		s1=s2;
+		System.out.println(s1+"  "+s2);
+		
+		
+	}
+
+}

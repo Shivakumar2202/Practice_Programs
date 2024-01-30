2eb5708d4427351cacf7770b68acd64e5cf47c41
Shivakumar A S <shivakumar42as@gmail.com>
2024-01-30 23:24:27 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ StringConcatination.java
@@ -0,0 +1,21 @@
+package String_progremms.level1;
+
+public class StringConcatination {
+
+	public static void main(String[] args) {
+		String s1="Shiva";
+		
+		String s2="kumar";
+		
+		String s3="A S";
+		
+		String s4=s1+s2+" "+s3;
+		System.out.println(s4);
+		
+		System.out.println("-----------------------------");
+		System.out.println(s1.concat(s2.concat(" "+s3)));
+		
+
+	}
+
+}

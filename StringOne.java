616c6a0905e0c0aa38876cafb7d8a9cb28fb034b
Shivakumar A S <shivakumar42as@gmail.com>
2024-02-02 23:02:42 +0530

    Practice programs in kodnest.
    Basic Programs practiced in KodNest Technologies.

---
+++ StringOne.java
@@ -0,0 +1,17 @@
+package String_progremms.level1;
+
+public class StringOne {
+
+	public static void main(String[] args) {
+		String s = "Kodnest";
+		System.out.println(s);
+
+		String t = new String("September Batch");
+		System.out.println(t);
+
+		char u[] = { '2', '0', '2', '3' };
+		String v = new String(u);
+		System.out.println(v);
+	}
+
+}
